473e4442f863298fe1ba143376372dca727ae56e
unknown <adityaviren@gmail.com>
2020-10-01 09:16:47 +0530

    added board

--- src/tictactoe/TicTacToeGame.java
+++ src/tictactoe/TicTacToeGame.java
@@ -5,5 +5,12 @@ public class TicTacToeGame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to TicTacToeGame");
+		uc1CreateBoard();
+	}
+	public static void uc1CreateBoard() {
+		char[] board = new char[10];
+		for(int i=1;i<10;i++) {
+			board[i]=Character.MIN_VALUE;
+		}
 	}
 }
