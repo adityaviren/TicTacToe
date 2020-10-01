57e90c9304b1854d61d2bdb54b0f9ac27374dbff
unknown <adityaviren@gmail.com>
2020-10-01 10:20:58 +0530

    added select element method

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,12 +1,22 @@
 package com.cg.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToeGame");
-		char board[] =createBoard();
+		char board[] = createBoard();
+		char player_element=selectElement();
+		char computer_element;
+		if(player_element=='X')
+			computer_element='O';
+		else
+			computer_element='X';
+		System.out.println("Player element is " + player_element + " and Computer element is " + computer_element);
+		
 	}
-	
+
 	/**
 	 * Created board
 	 */
@@ -16,5 +26,16 @@ public class TicTacToeGame {
 			board[iterator] = Character.MIN_VALUE;
 		}
 		return board;
+	}
+
+	/**
+	 * Select X or O for player
+	 */
+	public static char selectElement() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter X or O as your element");
+		char element = (sc.nextLine()).charAt(0);
+		sc.close();
+		return element;
 	}
 }
