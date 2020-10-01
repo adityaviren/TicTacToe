5a80100ccb58a895f9e5602140437997707a2587
unknown <adityaviren@gmail.com>
2020-10-01 17:53:39 +0530

    added firstTurn method

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -1,34 +1,22 @@
 package com.cg.tictactoe;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class TicTacToeGame {
 	static Scanner scanner = new Scanner(System.in);
 
-	private static char[] board = new char[10];
-
-	public static void main(String[] args) {
-		System.out.println("Welcome to TicTacToeGame");
-		char TicTacToeboard[] = createBoard();
-		char player_element = selectElement();
-		char computer_element = (player_element == 'X') ? 'O' : 'X';
-		System.out.println("Player element is " + player_element + " and Computer element is " + computer_element);
-		showBoard();
-		board[userMove()]=player_element;
-		showBoard();
-		scanner.close();
-
-	}
-
+	public static final int head = 1;
+	public static final int tail = 0;
+	public static enum Player {user,computer};
 	/**
 	 * Created board
 	 */
 	public static char[] createBoard() {
-
-		for (int iterator = 1; iterator < board.length; iterator++) {
-			board[iterator] = Character.MIN_VALUE;
+		char[] tictactoeboard = new char[10];
+		for (int iterator = 1; iterator < tictactoeboard.length; iterator++) {
+			tictactoeboard[iterator] = Character.MIN_VALUE;
 		}
-		return board;
+		return tictactoeboard;
 	}
 
 	/**
@@ -42,7 +30,7 @@ public class TicTacToeGame {
 	/**
 	 * Show the game board
 	 */
-	public static void showBoard() {
+	public static void showBoard(char[] board) {
 		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
 		System.out.println("---------");
 		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
@@ -53,32 +41,74 @@ public class TicTacToeGame {
 	/**
 	 * returns if index is empty
 	 */
-	public static boolean isEmpty(int index) {
-		return board[index] != ' ';
+	public static boolean isEmpty(char[] board, int index) {
+		return board[index] == ' ';
 	}
-	
+
 	/**
 	 * User enters move
 	 */
-	public static int userMove() {
-		boolean loop = true;
-		int index=0;
-		while (loop) {
-			System.out.println("Enter the index where you want to place marker");
-			index = Integer.parseInt(scanner.nextLine());
-			if (index >0 && index<10) {
-			boolean check = isEmpty(index);
-			if (check == false)
-				System.out.println("position not empty");
-			else {
-				System.out.println("position filled");
-				loop = false;
+	public static void userMove(char[] board, ArrayList<Integer> valid_positions, char player_element) {
+		while (true) {
+			System.out.println("Enter one of the unfilled positions");
+			int index = Integer.parseInt(scanner.nextLine());
+			if (valid_positions.contains(index)) {
+				board[index] = player_element;
+				valid_positions.remove(valid_positions.indexOf(index));
+				break;
 			}
-			}
-			else 
-				System.out.println("Enter valid index from 1 to 9");
 		}
-		return index;
+
+	}
+
+	/**
+	 * computer makes move
+	 */
+	public static void computerMove(char[] board, ArrayList<Integer> valid_positions, char computer_element) {
+		Random random = new Random();
+		int position = (int) (random.nextInt(valid_positions.size()) + 1);
+		board[position] = computer_element;
+		valid_positions.remove(valid_positions.indexOf(position));
+
+	}
+
+	/**
+	 * toss for first move
+	 */
+	public static Player firstTurn() {
+		Random random = new Random();
+		int toss = (int) random.nextInt(2);
+		return (toss == head) ? Player.user:Player.computer;
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to TicTacToeGame");
+		char board[] = createBoard();
+		char player_element = selectElement();
+		char computer_element = (player_element == 'X') ? 'O' : 'X';
+		System.out.println("Player element is " + player_element + " and Computer element is " + computer_element);
+		showBoard(board);
+		ArrayList<Integer> valid_positions = new ArrayList<Integer>();
+		for (int iterator = 1; iterator <= 9; iterator++) {
+			valid_positions.add(iterator);
+		}
+		Player player = firstTurn();
+		System.out.println(player + " gets the first turn");
+		for (int iterator = 1; iterator <= 3; iterator++) {
+			if (player==player.user) {
+				userMove(board, valid_positions, player_element);
+				showBoard(board);
+				computerMove(board, valid_positions, computer_element);
+				showBoard(board);
+			} else {
+				computerMove(board, valid_positions, computer_element);
+				showBoard(board);
+				userMove(board, valid_positions, player_element);
+				showBoard(board);
+			}
+			System.out.println(valid_positions);
+		}
+		scanner.close();
 	}
 
 }
