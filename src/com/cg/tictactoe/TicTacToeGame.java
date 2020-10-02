b3a309cfb08e9920f1fb09e80e2f78b5c6289895
unknown <adityaviren@gmail.com>
2020-10-02 13:42:37 +0530

    added isWinner method

--- src/com/cg/tictactoe/TicTacToeGame.java
+++ src/com/cg/tictactoe/TicTacToeGame.java
@@ -7,7 +7,7 @@ public class TicTacToeGame {
 
 	public static final int head = 1;
 	public static final int tail = 0;
-	public static enum Player {user,computer};
+
 	/**
 	 * Created board
 	 */
@@ -69,19 +69,29 @@ public class TicTacToeGame {
 		int position = (int) (random.nextInt(valid_positions.size()));
 		System.out.println(valid_positions.size());
 		System.out.println(position);
-		
+
 		board[valid_positions.get(position)] = computer_element;
-		valid_positions.remove(valid_positions.get(position));
+		valid_positions.remove(position);
 
 	}
 
 	/**
 	 * toss for first move
 	 */
-	public static Player firstTurn() {
+	public static int firstTurn() {
 		Random random = new Random();
 		int toss = (int) random.nextInt(2);
-		return (toss == head) ? Player.user:Player.computer;
+		return (toss == head) ? head : tail;
+	}
+
+	/**
+	 * check if winner by element
+	 */
+	public static boolean isWinner(char[] b, char el) {
+		return ((b[1] == b[2] && b[3] == b[2] && b[2] == el) || (b[4] == b[5] && b[5] == b[6] && b[5] == el)
+				|| (b[7] == b[8] && b[8] == b[9] && b[8] == el) || (b[1] == b[4] && b[4] == b[7] && b[4] == el)
+				|| (b[2] == b[5] && b[5] == b[8] && b[5] == el) || (b[3] == b[6] && b[6] == b[9] && b[6] == el)
+				|| (b[1] == b[5] && b[5] == b[9] && b[5] == el) || (b[3] == b[5] && b[5] == b[7] && b[5] == el));
 	}
 
 	public static void main(String[] args) {
@@ -95,24 +105,44 @@ public class TicTacToeGame {
 		for (int iterator = 1; iterator <= 9; iterator++) {
 			valid_positions.add(iterator);
 		}
-		Player player = firstTurn();
-		System.out.println(player + " gets the first turn");
-		for (int iterator = 1; iterator <= 9; iterator++) {
-			if (player==player.user) {
+		int toss = firstTurn();
+		String message = (toss == 1) ? "User" : "Computer";
+		System.out.println(message + " gets the first turn");
+
+		boolean is_winner = false,toggle;
+		toggle = (toss==1) ? true : false ;
+		int no_of_turns = 0;
+
+		while (valid_positions.size() > 0 && no_of_turns < 9) {
+			if (toggle) {
 				userMove(board, valid_positions, player_element);
 				showBoard(board);
-				System.out.println(valid_positions);
-				computerMove(board, valid_positions, computer_element);
-				showBoard(board);
-				System.out.println(valid_positions);
-			} else {
-				computerMove(board, valid_positions, computer_element);
-				showBoard(board);
-				System.out.println(valid_positions);
-				userMove(board, valid_positions, player_element);
-				showBoard(board);
-				System.out.println(valid_positions);
+				is_winner = isWinner(board, player_element);
+				if (is_winner == true) {
+					System.out.println("player won");
+					break;
+				}
+				no_of_turns++;
+				toggle = !toggle;
+				if(no_of_turns<9)
+				System.out.println("turn change");
 			}
+			else {
+				computerMove(board, valid_positions, computer_element);
+				showBoard(board);
+				is_winner = isWinner(board, computer_element);
+				if (is_winner == true) {
+					System.out.println("computer won");
+					break;
+				}
+				no_of_turns++;
+				toggle=!toggle;
+				if(no_of_turns<9)
+				System.out.println("turn change");
+			}
+			}
+		if(valid_positions.size()==0&&is_winner==false) {
+			System.out.println("the game tied");
 		}
 		scanner.close();
 	}
